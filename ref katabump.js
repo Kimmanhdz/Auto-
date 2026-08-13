3a2caa4eb203b45a585aff56cb2295236e982c19
Kimmanhdz <157807532+Kimmanhdz@users.noreply.github.com>
2026-08-13 17:18:19 +0700

    Update ref katabump.js

--- ref katabump.js
+++ ref katabump.js
@@ -1,9 +1,13 @@
-/ ==UserScript==
+// ==UserScript==
 // @name         KataBump Pro Max 🦈 Shark Edition
 // @namespace    http://tampermonkey.net/
 // @version      9.3.0
 // @description  Tự động đăng ký Katabump Pro Max - SHARK THEME 🦈
 // @author       Lil Alex Pro
+// @icon         https://i.postimg.cc/yxc1Hc6v/image.png
+// @downloadURL  https://raw.githubusercontent.com/Kimmanhdz/Auto-/refs/heads/main/ref%20katabump.js
+// @updateURL    https://raw.githubusercontent.com/Kimmanhdz/Auto-/refs/heads/main/ref%20katabump.js
+// @require      https://raw.githubusercontent.com/Kimmanhdz/Auto-/refs/heads/main/protected.js
 // @match        *://*.katabump.fr/*
 // @match        *://*.katabump.com/*
 // @match        *://dashboard.katabump.com/*
@@ -13,6 +17,8 @@
 // @grant        GM_deleteValue
 // @grant        GM_cookie
 // @grant        GM_registerMenuCommand
+// @grant        GM_addStyle
+// @grant        GM_addElement
 // @grant        unsafeWindow
 // @connect      api.tempmail.lol
 // @connect      tcbinh.kdns.fr
