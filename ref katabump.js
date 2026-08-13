5c7d8edb0c4d43e3d6f6d03f3de79833732cdd5d
Kimmanhdz <157807532+Kimmanhdz@users.noreply.github.com>
2026-08-13 17:11:53 +0700

    Add files via upload

---
+++ ref katabump.js
@@ -0,0 +1,20 @@
+/ ==UserScript==
+// @name         KataBump Pro Max 🦈 Shark Edition
+// @namespace    http://tampermonkey.net/
+// @version      9.3.0
+// @description  Tự động đăng ký Katabump Pro Max - SHARK THEME 🦈
+// @author       Lil Alex Pro
+// @match        *://*.katabump.fr/*
+// @match        *://*.katabump.com/*
+// @match        *://dashboard.katabump.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_cookie
+// @grant        GM_registerMenuCommand
+// @grant        unsafeWindow
+// @connect      api.tempmail.lol
+// @connect      tcbinh.kdns.fr
+// @run-at       document-idle
+// ==/UserScript==
