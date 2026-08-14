e73df059def2e3795b515785d888a94cd41bbe20
Kimmanhdz <157807532+Kimmanhdz@users.noreply.github.com>
2026-08-14 07:42:59 +0700

    Update user script metadata for KataBump

--- ref katabump.js
+++ ref katabump.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name         KataBump Pro Max 🦈 Shark Edition
+// @name         KataBump Shark Pro 🦈 + Gemini Captcha
 // @namespace    http://tampermonkey.net/
-// @version      9.3.0
-// @description  Tự động đăng ký Katabump Pro Max - SHARK THEME 🦈
-// @author       Lil Alex Pro
+// @version      3.2.0
+// @description  Tự động đăng ký Katabump + Giải Captcha bằng Gemini AI
+// @author       Shark Pro
 // @icon         https://i.postimg.cc/yxc1Hc6v/image.png
 // @downloadURL  https://raw.githubusercontent.com/Kimmanhdz/Auto-/refs/heads/main/ref%20katabump.js
 // @updateURL    https://raw.githubusercontent.com/Kimmanhdz/Auto-/refs/heads/main/ref%20katabump.js
-// @require      https://raw.githubusercontent.com/Kimmanhdz/Auto-/refs/heads/main/protected.js
 // @match        *://*.katabump.fr/*
 // @match        *://*.katabump.com/*
 // @match        *://dashboard.katabump.com/*
@@ -18,9 +17,9 @@
 // @grant        GM_cookie
 // @grant        GM_registerMenuCommand
 // @grant        GM_addStyle
-// @grant        GM_addElement
 // @grant        unsafeWindow
 // @connect      api.tempmail.lol
 // @connect      tcbinh.kdns.fr
+// @connect      generativelanguage.googleapis.com
 // @run-at       document-idle
 // ==/UserScript==
